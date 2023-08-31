1fb214011170a808f901931cf212e7669c04927c
Sushrut <sushrutathawale1509@gmail.com>
2023-08-31 20:09:48 +0530

    basic backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+app.listen(port , ()=>{
+    console.log("express working....")
+    console.log(`Listening on PORT ${port}`)
+})
