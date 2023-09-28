1dffcbcd057cf892e1b91a66e3e91e6bd38f918d
Sushrut <sushrutathawale1509@gmail.com>
2023-09-28 23:48:36 +0530

    db update

--- backend/config/dbConnections.js
+++ backend/config/dbConnections.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 const dbconnect = async() => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URL);
+        const db = await mongoose.connect(process.env.MONGO_URL,{
+            useUnifiedTopology: true,
+          });
         console.log("DB connected sucessfully..",db.connection.host,db.connection.name)
     } catch (err) {
         console.log(err);
